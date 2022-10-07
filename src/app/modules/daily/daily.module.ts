f7f8d2cb0695a5ee8b8ed096530239b4489e4f7e
djessyczaplicki <djessyczaplicki@gmail.com>
2022-10-07 14:26:45 +0200

    modified image

--- src/app/modules/daily/daily.module.ts
+++ src/app/modules/daily/daily.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DailyRoutingModule } from './daily-routing.module';
-import { DailyComponent } from './pages';
-import { TitleComponent } from './components/title/title.component';
 import { AddMemberComponent } from './components/add-member/add-member.component';
+import { DailyComponent } from './pages';
+import { DailyRoutingModule } from './daily-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MemberListComponent } from './components/member-list/member-list.component';
-import { TimeSetterComponent } from './components/time-setter/time-setter.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MemberTimesComponent } from './components/member-times/member-times.component';
-import { TimerComponent } from './components/timer/timer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ResultsComponent } from './components/results/results.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TimerComponent } from './components/timer/timer.component';
+import { TimeSetterComponent } from './components/time-setter/time-setter.component';
+import { TitleComponent } from './components/title/title.component';
 @NgModule({
   declarations: [
     DailyComponent,
